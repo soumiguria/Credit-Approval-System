132fb172b5045dbb70143c4eb318c9e5e2467517
soumi guria <soumiguria.cse2022@nsec.ac.in>
2023-12-19 22:48:13 +0530

    updatee

--- credit/tests.py
+++ credit/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.contrib import admin
+from django.urls import path, include
 
-# Create your tests here.
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('credit.urls')),  # Include the URLs from your app
+    # Add the following line to handle the root path
+    path('', include('credit.urls')),  # Redirect to your app's URLs for the root path
+]
--- credit/urls.py
+++ credit/urls.py
@@ -1,18 +1,12 @@
 # testproject/urls.py
 
 from django.urls import path
-from .views import (
-    register_customer,
-    check_loan_eligibility,
-    create_loan,
-    view_loan,
-    view_loans_by_customer,
-)
+from . import views
 
 urlpatterns = [
-    path('register/', register_customer, name='register_customer'),
-    path('check-eligibility/', check_loan_eligibility, name='check_loan_eligibility'),
-    path('create-loan/', create_loan, name='create_loan'),
-    path('view-loan/<int:loan_id>/', view_loan, name='view_loan'),
-    path('view-loans/<int:customer_id>/', view_loans_by_customer, name='view_loans_by_customer'),
+    path('register/', views.register_customer, name='register_customer'),
+    path('check-eligibility/', views.check_loan_eligibility, name='check_loan_eligibility'),
+    path('create-loan/', views.create_loan, name='create_loan'),
+    path('view-loan/<int:loan_id>/', views.view_loan, name='view_loan'),
+    path('view-loans/<int:customer_id>/', views.view_loans_by_customer, name='view_loans_by_customer'),
 ]
